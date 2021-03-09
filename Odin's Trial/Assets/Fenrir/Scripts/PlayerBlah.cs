c5a55ba0ed9d7d439f3c8e08fa5ed3e80a9e6e3d
Tiggi Pengelly <pengelly@sheridancollege.ca>
2021-03-09 17:39:13 -0500

    Fenrir scripts/prefab and a cone script!

---
+++ Odin's Trial/Assets/Fenrir/Scripts/PlayerBlah.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlah : MonoBehaviour
+{
+    public bool alive = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "eyes"){
+            other.transform.GetComponent<Fenrirs_Brain>().CheckSight();            
+        }
+
+    }
+}
