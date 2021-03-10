34c28487d9d1d28759b341c195260d57b0e60988
Kate Jarvis <80274052+Jarvikat@users.noreply.github.com>
2021-03-10 15:50:07 -0500

    Torch function added
    Torch can now be lit when the player is within distance of the firepit

---
+++ Odin's Trial/Assets/Scripts/Firepit & Torch Scripts/torchScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torchScript : MonoBehaviour
+{
+    public ParticleSystem fire;
+    public playerInteract player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire = GetComponent<ParticleSystem>();
+        fire.Stop();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightFire();
+    }
+
+    void lightFire() 
+    {
+
+        if (Input.GetKeyDown(KeyCode.F) && player.playeriswithDistance == true) 
+        {
+
+
+            Debug.Log("Light em up");
+            fire.Play();
+
+
+        }
+    
+    
+    }
+
+}
