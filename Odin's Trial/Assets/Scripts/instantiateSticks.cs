0359f6e9c928a1f803b68aabe2f6f3bc56c195ed
Kate Jarvis <80274052+Jarvikat@users.noreply.github.com>
2021-03-10 17:07:05 -0500

    Began work on instantiating sticks
    Started the code to instantiate the sticks
    Changed the ui font

---
+++ Odin's Trial/Assets/Scripts/instantiateSticks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateSticks : MonoBehaviour
+{
+    public GameObject Prefab;
+    private int minxrange = 300;
+    private int maxxrange = 700;
+
+    private List<Transform> _instances = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject wood = Instantiate(Prefab, new Vector3(transform.position.x + 1, 0, transform.position.z + 1)) as GameObject;
+            wood.transform.parent = transform;
+        }
+    }
+}
