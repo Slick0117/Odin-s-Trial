acdd0d43e25e1f3a08932774b7ad60825d65c59a
Kate Jarvis <80274052+Jarvikat@users.noreply.github.com>
2021-03-11 13:15:06 -0500

    Added in safe area to firepit

---
+++ Odin's Trial/Assets/Scripts/Firepit & Torch Scripts/firepitSafeArea.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firepitSafeArea : MonoBehaviour
+{
+    public fireLife firepit;
+    public Collider safe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        safe = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnoffSafeSpace();
+    }
+
+    void turnoffSafeSpace()
+    {
+
+
+        if (firepit.fireisOn == true)
+        {
+
+
+            safe.enabled = true;
+
+
+        }
+
+        if (firepit.fireisOn == false)
+        {
+
+
+            safe.enabled = false;
+
+        }
+
+    }
+}
